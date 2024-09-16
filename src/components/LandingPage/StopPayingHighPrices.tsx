2e47bb3cb5bbd7c112db1989dff26e114cc5d728
Matías Galeano <matiasmgpersonal@outlook.com>
2024-09-16 17:59:48 -0300

    added link

--- src/components/LandingPage/StopPayingHighPrices.tsx
+++ src/components/LandingPage/StopPayingHighPrices.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function StopPayingHighPrices() {
   return <div className="flex justify-around h-[24rem] bg-gradient-to-r from-blue-500 to-blue-400">
     <div className="uppercase text-white font-semibold flex flex-col gap-4 justify-center">
@@ -8,7 +10,9 @@ export default function StopPayingHighPrices() {
         <p>Te llega en menos de 7 días</p>
       </div>
       <h3 className="text-2xl font-bold">¿Qué estás esperando?</h3>
-      <button className="py-2 border-4 text-lg border-white bg-[#ffffff50] transition-all hover:bg-[#ffffff70] text-black w-[14rem]">Ver productos</button>
+      <Link to="/products">
+        <button className="py-2 border-4 text-lg border-white bg-[#ffffff50] transition-all hover:bg-[#ffffff70] text-black w-[14rem]">Ver productos</button>
+      </Link>
     </div>
     <img src="prices.png" alt="Deja de pagar precios elevados" />
   </div>
