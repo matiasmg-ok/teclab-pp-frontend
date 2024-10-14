55bff3667f7d0e8300d5d1a0dce91fc84e0eae7e
Matías Galeano <matiasmgpersonal@outlook.com>
2024-10-13 22:41:22 -0300

    adding details to my orders

--- src/pages/dashboard/Orders.tsx
+++ src/pages/dashboard/Orders.tsx
@@ -1,21 +1,57 @@
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
+import { Order } from "../../types/Order";
 import { useClient } from "../../utils/loggedClient";
+
+const status = {
+  'payment-pending': 'Pago pendiente de confirmación',
+  'payment-completed': 'Pagado',
+  'in-progress': 'En proceso',
+  'shipping': 'Enviado',
+  'finished': 'Finalizado',
+  'cancelled': 'Cancelado'
+}
+
+function OrderCard({ order }: { order: Order }) {
+
+  const product = order.products[0].product;
+
+  return <>
+    <div className={'flex items-center w-6/12 min-h-[15rem] border-4 border-blue-200'}>
+      <img className="w-[10rem] h-[10rem] mx-10" src={`${import.meta.env.VITE_BACKEND_URL}/${product.imageUrl}`} alt={product.name} />
+      <div className="self-start mt-10">
+        <h1 className="text-xl font-semibold">{product.name}</h1>
+        <p>Comprado el {new Date(order.createdAt).toLocaleDateString()}</p>
+        <p>Estado: {status[order.status]}</p>
+        <p>Código de tracking: {order.trackingCode}</p>
+        {
+          order.status === 'payment-pending' && <>
+            <p>Moneda elegida: {order.currency === 'usd' ? 'Dólar estadounidense' : 'Peso argentino'}</p>
+            <p>Monto total: ${order.price}</p>
+            <button onClick={()=>{}} className="w-[12rem] py-1 px-2 bg-blue-400 rounded-sm text-white mt-5 font-medium text-md hover:bg-blue-500 transition-all">Ver datos para pagar</button>
+          </>
+        }
+      </div>
+    </div>
+  </>
+}
 
 export default function Orders() {
 
   const [{ data: user }] = useClient('/users/whoami');
-  const [{ data: orders }] = useClient('/orders/self');
+  const [{ data: orders }] = useClient<Order[]>('/orders/self');
 
   return user && orders && <>
     <Navbar />
     <div className="font-roboto">
       <h1 className="text-2xl font-semibold text-center">Mis compras</h1>
-      {
-        orders.map((order) => {
-          
-        })
-      }
+      <div className="flex flex-col items-center mt-10">
+        {
+          orders.map((order) => {
+            return <OrderCard key={order.id} order={order} />
+          })
+        }
+      </div>
     </div>
     <Footer />
   </>
--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,49 +1,9 @@
-/*
-import { Column, Entity, ManyToOne, OneToMany } from "typeorm";
-import { DefaultData } from "./DefaultData";
 import { Product } from "./Product";
-import { User } from "./User";
-import { OrderProduct } from "./OrderProduct";
-
-@Entity("orders")
-export class Order extends DefaultData {
-  @ManyToOne(() => User, (user) => user.orders)
-  user: User;
-
-  @Column("varchar", { length: 100, default: 'payment-pending' })
-  status: 'payment-pending' | 'payment-completed' | 'in-progress' | 'shipping' | 'finished' | 'cancelled';
-
-  @Column("text")
-  shippingAddress: string;
-
-  @Column("text")
-  trackingCode: string;
-
-  @Column("text")
-  additionalNotes: string;
-
-  @Column("varchar", { length: 150 })
-  city: string;
-
-  @Column("varchar", { length: 150 })
-  province: string;
-
-  @Column("varchar", { length: 150 })
-  zip: string;
-
-  @Column('decimal', { precision: 5, scale: 2 })
-  price: number;
-
-  @Column('varchar', { length: 50 })
-  currency: 'usd' | 'ars';
-
-  @OneToMany(() => OrderProduct, orderProduct => orderProduct.order)
-  products: OrderProduct[]
-}
-*/
 
 export type Order = {
+  id: number,
   status: 'payment-pending' | 'payment-completed' | 'in-progress' | 'shipping' | 'finished' | 'cancelled',
+  createdAt: Date,
   shippingAddress: string,
   trackingCode: string,
   additionalNotes: string,
@@ -52,4 +12,7 @@ export type Order = {
   zip: string,
   price: number,
   currency: 'usd' | 'ars',
+  products: {
+    product: Product
+  }[],
 }
